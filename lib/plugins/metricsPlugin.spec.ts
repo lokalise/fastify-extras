dfcd0508aaeb6dc7ceaf04846dfc464f34f16385
Igor Savin <iselwin@gmail.com>
2023-01-02 17:43:01 +0200

    Prometheus metrics plugin (#7)

---
+++ lib/plugins/metricsPlugin.spec.ts
@@ -0,0 +1,54 @@
+import { sendGet } from '@lokalise/node-core'
+import type { FastifyInstance } from 'fastify'
+import fastify from 'fastify'
+
+import { metricsPlugin } from './metricsPlugin'
+
+async function initApp(errorObjectResolver = (err: unknown) => err) {
+  const app = fastify()
+  await app.register(metricsPlugin, {
+    bindAddress: '0.0.0.0',
+    loggerOptions: false,
+    errorObjectResolver,
+  })
+
+  await app.ready()
+  return app
+}
+
+describe('metricsPlugin', () => {
+  let app: FastifyInstance
+  beforeAll(async () => {
+    app = await initApp()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('returns Prometheus metrics', async () => {
+    const response = await sendGet('http://127.0.0.1:9080', '/metrics')
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual(expect.any(String))
+  })
+
+  it('handles an error', async () => {
+    expect.assertions(2)
+    let handledError
+    try {
+      await initApp((err) => {
+        handledError = err
+        return err
+      })
+    } catch (err) {
+      // eslint-disable-next-line jest/no-conditional-expect
+      expect(err).toMatchObject({
+        message: 'Critical error when trying to launch metrics server',
+      })
+    }
+
+    expect(handledError).toMatchObject({
+      code: 'EADDRINUSE',
+    })
+  })
+})
---
+++ lib/plugins/metricsPlugin.ts
@@ -0,0 +1,66 @@
+import type { FastifyInstance } from 'fastify'
+import fastify from 'fastify'
+import fastifyMetrics from 'fastify-metrics'
+import fp from 'fastify-plugin'
+import type { PinoLoggerOptions } from 'fastify/types/logger'
+
+const METRICS_PORT = 9080
+
+export type ErrorObjectResolver = (err: unknown, correlationID?: string) => unknown
+
+export interface MetricsPluginOptions {
+  loggerOptions: PinoLoggerOptions | boolean
+  disablePrometheusRequestLogging?: boolean
+  bindAddress?: string
+  errorObjectResolver: ErrorObjectResolver
+}
+
+function plugin(app: FastifyInstance, opts: MetricsPluginOptions) {
+  void app.register(fastifyMetrics, {
+    defaultMetrics: { enabled: true },
+    endpoint: null,
+    clearRegisterOnInit: true,
+    name: 'metrics',
+    routeMetrics: { enabled: true },
+  })
+
+  const promServer = fastify({
+    logger: opts.loggerOptions,
+    disableRequestLogging: opts.disablePrometheusRequestLogging ?? true,
+  })
+
+  promServer.route({
+    url: '/metrics',
+    method: 'GET',
+    logLevel: 'info',
+    schema: {
+      // hide route from swagger plugins
+      // @ts-expect-error
+      hide: true,
+    },
+    handler: async (_, reply) => {
+      const metrics = await app.metrics.client.register.metrics()
+      return reply.type('text/plain').send(metrics)
+    },
+  })
+
+  app.addHook('onClose', async () => {
+    await promServer.close()
+  })
+
+  return promServer
+    .listen({
+      port: METRICS_PORT,
+      host: opts.bindAddress,
+    })
+    .catch((err) => {
+      const logObject = opts.errorObjectResolver(err)
+      promServer.log.error(logObject)
+      throw new Error('Critical error when trying to launch metrics server')
+    })
+}
+
+export const metricsPlugin = fp(plugin, {
+  fastify: '4.x',
+  name: 'metrics-plugin',
+})
