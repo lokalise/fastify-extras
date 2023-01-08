f6702ea2993a05c5d9226bcc6f49ee96645e7745
Igor Savin <iselwin@gmail.com>
2023-01-08 14:12:42 +0200

    Implement plugin for prisma/OpenTracing support

--- lib/index.ts
+++ lib/index.ts
@@ -16,3 +16,6 @@ export type { NewRelicTransactionManagerOptions } from './plugins/newrelicTransa
 
 export { metricsPlugin } from './plugins/metricsPlugin'
 export type { ErrorObjectResolver, MetricsPluginOptions } from './plugins/metricsPlugin'
+
+export { prismaOpenTracingPlugin } from './plugins/opentelemetry/opentelemetry/prismaOpenTracingPlugin'
+export type { PrismaOpenTracingPluginConfig } from './plugins/opentelemetry/opentelemetry/prismaOpenTracingPlugin'
