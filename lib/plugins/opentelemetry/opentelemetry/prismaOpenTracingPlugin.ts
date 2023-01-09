381bc5a72438c5df7f55cfb65ae42dbb0b418d4f
Igor Savin <iselwin@gmail.com>
2023-01-09 11:34:58 +0200

    Implement plugin for prisma/OpenTracing support (#11)
    Co-authored-by: Andreas Kristiansen <110380682+lokankr@users.noreply.github.com>

---
+++ lib/plugins/opentelemetry/opentelemetry/prismaOpenTracingPlugin.ts
@@ -0,0 +1,53 @@
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc'
+import { registerInstrumentations } from '@opentelemetry/instrumentation'
+import { Resource } from '@opentelemetry/resources'
+import { TraceIdRatioBasedSampler } from '@opentelemetry/sdk-trace-base'
+import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node'
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
+import { PrismaInstrumentation } from '@prisma/instrumentation'
+import type { FastifyInstance } from 'fastify'
+import fp from 'fastify-plugin'
+
+import { LokaliseBatchSpanProcessor, LokaliseSimpleSpanProcessor } from './spanProcessors'
+
+export interface PrismaOpenTracingPluginConfig {
+  isEnabled: boolean
+  useBatchSpans: boolean
+  samplingRatio: number
+  serviceName: string
+}
+
+function plugin(app: FastifyInstance, opts: PrismaOpenTracingPluginConfig, done: () => void) {
+  if (opts.isEnabled) {
+    const provider = new NodeTracerProvider({
+      sampler: new TraceIdRatioBasedSampler(opts.samplingRatio),
+      resource: new Resource({
+        [SemanticResourceAttributes.SERVICE_NAME]: opts.serviceName,
+      }),
+    })
+
+    // Is configured by OTEL_EXPORTER_OTLP_* env vars
+    const otlpExporter = new OTLPTraceExporter()
+
+    // Production sends spans in batches
+    if (opts.useBatchSpans) {
+      provider.addSpanProcessor(new LokaliseBatchSpanProcessor(otlpExporter))
+    } else {
+      provider.addSpanProcessor(new LokaliseSimpleSpanProcessor(otlpExporter))
+    }
+
+    registerInstrumentations({
+      instrumentations: [new PrismaInstrumentation()],
+      tracerProvider: provider,
+    })
+
+    provider.register()
+  }
+
+  done()
+}
+
+export const prismaOpenTracingPlugin = fp(plugin, {
+  fastify: '4.x',
+  name: 'prisma-otel-tracing-plugin',
+})
