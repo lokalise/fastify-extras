e80ec84b2b3b6df2d88371c357e2000ce63071c5
Igor Savin <iselwin@gmail.com>
2023-04-17 13:44:55 +0300

    Make healthcheck more useful

--- lib/plugins/publicHealthcheckPlugin.spec.ts
+++ lib/plugins/publicHealthcheckPlugin.spec.ts
@@ -1,10 +1,17 @@
 import type { FastifyInstance } from 'fastify'
 import fastify from 'fastify'
 
-import type { PublicHealthcheckPluginOptions } from './publicHealthcheckPlugin'
+import type { HealthCheck, PublicHealthcheckPluginOptions } from './publicHealthcheckPlugin'
 import { publicHealthcheckPlugin } from './publicHealthcheckPlugin'
 
-async function initApp(opts?: PublicHealthcheckPluginOptions) {
+const positiveHealthcheck: HealthCheck = () => {
+  return Promise.resolve(true)
+}
+const negativeHealthcheck: HealthCheck = () => {
+  return Promise.resolve(false)
+}
+
+async function initApp(opts: PublicHealthcheckPluginOptions) {
   const app = fastify()
   await app.register(publicHealthcheckPlugin, opts)
   await app.ready()
@@ -18,18 +25,40 @@ describe('publicHealthcheckPlugin', () => {
   })
 
   it('returns a heartbeat', async () => {
-    app = await initApp()
+    app = await initApp({ healthChecks: [] })
 
     const response = await app.inject().get('/').end()
     expect(response.statusCode).toBe(200)
-    expect(response.json()).toEqual({ status: 'OK' })
+    expect(response.json()).toEqual({ heartbeat: 'HEALTHY' })
   })
 
   it('returns custom heartbeat', async () => {
-    app = await initApp({ responsePayload: { version: 1 } })
+    app = await initApp({ responsePayload: { version: 1 }, healthChecks: [] })
 
     const response = await app.inject().get('/').end()
     expect(response.statusCode).toBe(200)
-    expect(response.json()).toEqual({ version: 1 })
+    expect(response.json()).toEqual({ heartbeat: 'HEALTHY', version: 1 })
+  })
+
+  it('returns false if one healthcheck fails', async () => {
+    app = await initApp({
+      responsePayload: { version: 1 },
+      healthChecks: [negativeHealthcheck, positiveHealthcheck],
+    })
+
+    const response = await app.inject().get('/').end()
+    expect(response.statusCode).toBe(200)
+    expect(response.json()).toEqual({ heartbeat: 'FAIL', version: 1 })
+  })
+
+  it('returns true if all healthchecks pass', async () => {
+    app = await initApp({
+      responsePayload: { version: 1 },
+      healthChecks: [positiveHealthcheck, positiveHealthcheck],
+    })
+
+    const response = await app.inject().get('/').end()
+    expect(response.statusCode).toBe(200)
+    expect(response.json()).toEqual({ heartbeat: 'HEALTHY', version: 1 })
   })
 })
