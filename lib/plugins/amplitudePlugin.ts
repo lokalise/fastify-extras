17fc0ec4b38a5baeef690b762f0e446e2e9caafd
CarlosGamero <carlos.gamero@lokalise.com>
2023-07-04 14:13:42 +0200

    AP-21 Adding doc

--- lib/plugins/amplitudePlugin.ts
+++ lib/plugins/amplitudePlugin.ts
@@ -14,16 +14,6 @@ import type {
 } from 'fastify'
 import fp from 'fastify-plugin'
 
-export type createApiTrackingEventFn = (req: FastifyRequest, res: FastifyReply) => BaseEvent | null
-
-export interface AmplitudeConfig {
-  isEnabled: boolean
-  apiKey?: string
-  options?: NodeOptions
-  apiUsageTracking?: createApiTrackingEventFn
-  plugins?: Plugin[]
-}
-
 async function plugin(
   fastify: FastifyInstance,
   config: AmplitudeConfig,
@@ -66,9 +56,65 @@ function enableApiUsageTracking(
   )
 }
 
+/**
+ * Callback used to create the events that will be used to automatically
+ * track the API usage
+ */
+export type createApiTrackingEventFn = (req: FastifyRequest, res: FastifyReply) => BaseEvent | null
+
+/**
+ * Configuration to set up the Amplitude Plugin.
+ *
+ * @property {boolean} isEnabled Flag to enable or disable the plugin.
+ *
+ * @property {string} apiKey Amplitude api key, please get it from your
+ * Amplitude project
+ *
+ * @property {NodeOptions} Amplitude configuration, please check
+ * [this](https://amplitude.github.io/Amplitude-TypeScript/modules/_amplitude_analytics_node.Types.html#NodeOptions)
+ * to learn more.
+ *
+ * @property {createApiTrackingEventFn} apiUsageTracking Callback used to create
+ * the event that will be send automatically to track the API usage. If not
+ * specified the API usage track will be disabled.
+ *
+ * @property {Plugin[]} plugins Allow to extend plugin behavior by, for example,
+ * modifying event properties. Please check
+ * [this](https://www.docs.developers.amplitude.com/data/sdks/typescript-node/#plugins)
+ * to see how it works
+ */
+export interface AmplitudeConfig {
+  isEnabled: boolean
+  apiKey?: string
+  options?: NodeOptions
+  apiUsageTracking?: createApiTrackingEventFn
+  plugins?: Plugin[]
+}
+
+/**
+ * Use this method to register the amplitude plugin on your fastify instance.
+ *
+ * Example of usage:
+ * ```ts
+ * await app.register(amplitudePlugin, {
+ *     isEnabled: true,
+ *     apiKey: 'dummy-api-key',
+ *     options: {
+ *       serverZone: 'EU',
+ *     },
+ * })
+ * ```
+ */
 export const amplitudePlugin = fp<AmplitudeConfig>(plugin, {
   fastify: '4.x',
   name: 'amplitude-plugin',
 })
 
+/**
+ * Sends the given event to Amplitude
+ *
+ * @param event Event to send to amplitude. Please check
+ * [this](https://amplitude.github.io/Amplitude-TypeScript/interfaces/_amplitude_analytics_node.Types.BaseEvent.html)
+ * to get more info about the BaseEvent type
+ */
 export const amplitudeTrack = (event: BaseEvent): AmplitudeReturn<Result> => track(event)
