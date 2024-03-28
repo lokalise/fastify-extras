ea514dd09845e84dbc2bb4645e3f8c1bdb155e21
Bartosz Drozd <bartosz.drozd@lokalise.com>
2024-03-28 11:30:16 +0100

    chore: Reexport BugSnag related utils from error-utils package for backwards compatibility

--- lib/plugins/bugsnagPlugin.ts
+++ lib/plugins/bugsnagPlugin.ts
@@ -1,44 +1,22 @@
-import type { Event, NotifiableError } from '@bugsnag/js'
 import Bugsnag from '@bugsnag/js'
 import type { NodeConfig } from '@bugsnag/node'
-import type { ErrorReporter } from '@lokalise/node-core'
+import {
+  bugsnagErrorReporter,
+  type ErrorReport,
+  reportErrorToBugsnag,
+  type Severity,
+} from '@lokalise/error-utils'
 import type { FastifyInstance } from 'fastify'
 import fp from 'fastify-plugin'
 
-export type Severity = Event['severity']
-
-export interface ErrorReport {
-  error: NotifiableError
-  severity?: Severity
-  unhandled?: boolean
-  context?: Record<string, unknown>
-}
-
-export const reportErrorToBugsnag = ({
-  error,
-  severity = 'error',
-  unhandled = true,
-  context,
-}: ErrorReport) =>
-  Bugsnag.isStarted() &&
-  Bugsnag.notify(error, (event) => {
-    event.severity = severity
-    event.unhandled = unhandled
-    if (context) {
-      event.addMetadata('Context', context)
-    }
-  })
+export { reportErrorToBugsnag, bugsnagErrorReporter, ErrorReport, Severity }
 
 export interface BugsnagPluginConfig {
   bugsnag: NodeConfig
   isEnabled: boolean
 }
 
-export const bugsnagErrorReporter: ErrorReporter = {
-  report: (report) => reportErrorToBugsnag(report),
-}
-
-function plugin(app: FastifyInstance, opts: BugsnagPluginConfig, done: () => void) {
+function plugin(_app: FastifyInstance, opts: BugsnagPluginConfig, done: () => void) {
   if (opts.isEnabled) {
     Bugsnag.start(opts.bugsnag)
   }
