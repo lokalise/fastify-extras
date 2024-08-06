97040a2bc4ea91870dc81d5aabdb9646a68c321a
Kamil Wylegala <kamil.wylegala@gmail.com>
2024-08-06 16:02:46 +0200

    AP-4667 PreHandler for static tokens. (#180)
    * AP-4667 PreHandler for static tokens.
    
    * AP-4667 Readme.
    
    * AP-4667 Linted package.json
    
    * AP-4667 test case.
    
    * AP-4667 lint.
    
    * AP-4667 LoggerProvider param.

---
+++ lib/route-utils/authPreHandlers.spec.ts
@@ -0,0 +1,73 @@
+import { globalLogger } from '@lokalise/node-core'
+import fastify, { type FastifyInstance, type FastifyReply, type FastifyRequest } from 'fastify'
+import { beforeEach, describe } from 'vitest'
+import { createErrorHandler } from '../errors/errorHandler'
+import { createStaticTokenAuthPreHandler } from './authPreHandlers'
+
+const SECRET_TOKEN = 'my_secret'
+
+describe('authPreHandlers', () => {
+  let app: FastifyInstance
+
+  beforeEach(async () => {
+    app = await fastify()
+
+    app.route({
+      method: 'GET',
+      url: '/',
+      preHandler: createStaticTokenAuthPreHandler(SECRET_TOKEN, (_req) => globalLogger),
+      handler: (_req: FastifyRequest, res: FastifyReply) => {
+        res.status(200).send({
+          data: 'ok',
+        })
+      },
+    })
+    //For showing 4xx errors in pre handler throws error (instead of 500).
+    app.setErrorHandler(
+      createErrorHandler({
+        errorReporter: { report: () => {} },
+      }),
+    )
+
+    await app.ready()
+  })
+
+  it('accepts request if secret token provided in request is valid', async () => {
+    const response = await app
+      .inject()
+      .get('/')
+      .headers({
+        authorization: `Bearer ${SECRET_TOKEN}`,
+      })
+      .end()
+
+    expect(response.statusCode).toBe(200)
+    expect(response.json()).toEqual({
+      data: 'ok',
+    })
+  })
+
+  it('rejects with 401 if no token', async () => {
+    const response = await app.inject().get('/').end()
+    expect(response.statusCode).toBe(401)
+    expect(response.json()).toEqual({
+      errorCode: 'AUTH_FAILED',
+      message: 'Authentication failed',
+    })
+  })
+
+  it('rejects with 401 if invalid token', async () => {
+    const response = await app
+      .inject()
+      .get('/')
+      .headers({
+        authorization: 'bearer invalid_token',
+      })
+      .end()
+    expect(response.statusCode).toBe(401)
+    expect(response.json()).toEqual({
+      errorCode: 'AUTH_FAILED',
+      message: 'Authentication failed',
+    })
+  })
+})
