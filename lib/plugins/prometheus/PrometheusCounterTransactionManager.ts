4f820428e2dde87d13ace2cb8225b5b786994361
CarlosGamero <carlos.gamero@lokalise.com>
2024-09-03 17:03:56 +0200

    AP-5078 PrometheusCounterTransactionManager registering counter

--- lib/plugins/prometheus/PrometheusCounterTransactionManager.ts
+++ lib/plugins/prometheus/PrometheusCounterTransactionManager.ts
@@ -1,15 +1,26 @@
-import { Counter } from "prom-client";
+import type { Counter } from "prom-client";
 import type { TransactionObservabilityManager } from "@lokalise/node-core";
-import { undefined } from "zod";
+import type { IFastifyMetrics } from "fastify-metrics";
 
+/**
+ * TransactionObservabilityManager implementation that uses Prometheus counter
+ * to track the number of started, failed and success transactions.
+ */
 export class PrometheusCounterTransactionManager implements TransactionObservabilityManager {
-  private readonly isEnabled: boolean;
+  private readonly metricName: string
+  private readonly metricDescription: string
   private readonly counter?: Counter<'status' | 'transactionName'>;
 
   private readonly transactionNameById: Map<string, string> = new Map()
 
-  constructor() {
-    this.isEnabled = true
+  constructor(
+    metricName: string,
+    metricDescription:  string,
+    appMetrics?: IFastifyMetrics,
+  ) {
+    this.metricName = metricName
+    this.metricDescription = metricDescription
+    this.counter = this.registerMetric(appMetrics)
   }
 
   start(transactionName: string, uniqueTransactionKey: string): void {
@@ -22,11 +33,26 @@ export class PrometheusCounterTransactionManager implements TransactionObservabi
     this.counter?.inc({ status: 'started', transactionName })
   }
 
-  stop(uniqueTransactionKey: string, wasSuccessful: boolean = true): void {
+  stop(uniqueTransactionKey: string, wasSuccessful = true): void {
     const transactionName = this.transactionNameById.get(uniqueTransactionKey)
     if (!transactionName) return
 
     this.counter?.inc({ status: wasSuccessful ? 'success' : 'failed', transactionName })
     this.transactionNameById.delete(uniqueTransactionKey)
+  }
+
+  private registerMetric(appMetrics?: IFastifyMetrics) {
+    if (!appMetrics) return
+
+    const existingMetric: Counter| undefined =
+      appMetrics.client.register.getSingleMetric(this.metricName) as Counter | undefined
+
+    if (existingMetric) return existingMetric
+
+    return new appMetrics.client.Counter({
+      name: this.metricName,
+      help: this.metricDescription,
+      labelNames: ['status', 'transactionName'],
+    })
   }
 }
