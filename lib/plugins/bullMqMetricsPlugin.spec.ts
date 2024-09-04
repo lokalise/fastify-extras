d22d96db40507bf97b995e6c67d21b5fd0cb2e90
CarlosGamero <carlos.gamero@lokalise.com>
2024-09-04 10:34:09 +0200

    Increasing wait time

--- lib/plugins/bullMqMetricsPlugin.spec.ts
+++ lib/plugins/bullMqMetricsPlugin.spec.ts
@@ -167,7 +167,7 @@ describe('bullMqMetricsPlugin', () => {
     })
 
     await processor.spy.waitForJobWithId(jobId, 'completed')
-    await setTimeout(100)
+    await setTimeout(200)
 
     await app.bullMqMetrics.collect()
     const responseAfter = await getMetrics()
