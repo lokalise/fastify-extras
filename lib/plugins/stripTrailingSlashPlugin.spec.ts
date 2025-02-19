69ff1171a6f53ee4314236e47c53ed4b255435ad
Mateusz Tkacz <mateusz.tkacz@lokalise.com>
2025-02-19 12:46:07 +0100

    feat: add stripTrailingSlashPlugin

---
+++ lib/plugins/stripTrailingSlashPlugin.spec.ts
@@ -0,0 +1,46 @@
+import type { FastifyInstance } from 'fastify'
+import fastify from 'fastify'
+
+import { stripTrailingSlashPlugin } from './stripTrailingSlashPlugin'
+
+const initApp = async () => {
+  const app = fastify()
+
+  await app.register(stripTrailingSlashPlugin)
+
+  await app.ready()
+
+  return app
+}
+
+describe('stripTrailingSlashPlugin', () => {
+  let app: FastifyInstance
+
+  beforeAll(async () => {
+    app = await initApp()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('does not redirect on root', async () => {
+    const response = await app.inject().get('/').end()
+
+    expect(response.statusCode).not.toBe(302)
+  })
+
+  it('redirect when url ends with slash', async () => {
+    const response = await app.inject().get('/some/').end()
+
+    expect(response.statusCode).toBe(302)
+    expect(response.headers.location).toBe('/some')
+  })
+
+  it('redirect when path ends with slash and url has query params', async () => {
+    const response = await app.inject().get('/some/?a=b').end()
+
+    expect(response.statusCode).toBe(302)
+    expect(response.headers.location).toBe('/some?a=b')
+  })
+})
