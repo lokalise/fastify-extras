4a5c18483481cf51327772ce6c41378a4a524230
Mateusz Tkacz <mateusz.tkacz@lokalise.com>
2025-02-21 10:58:35 +0100

    chore: improve trailing slash plugin reliability

--- lib/plugins/stripTrailingSlashPlugin.ts
+++ lib/plugins/stripTrailingSlashPlugin.ts
@@ -3,9 +3,9 @@ import fp from 'fastify-plugin'
 
 const pluginCallback: FastifyPluginCallback = (fastify, _options, done) => {
   fastify.addHook('onRequest', (request, reply, done) => {
-    const { pathname, search } = new URL(
-      `${request.protocol}://${request.hostname}:${request.port}${request.url}`,
-    )
+    // Using a hardcoded origin for simplicity and reliability,
+    // as the request's origin is not relevant in this case.
+    const { pathname, search } = new URL(request.url, 'https://example.com')
 
     if (pathname.length > 1 && pathname.endsWith('/')) {
       reply.redirect(`${pathname.slice(0, -1)}${search}`, 302)
